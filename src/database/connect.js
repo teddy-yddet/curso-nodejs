4fe852601113fc4eeedf8bb4b95de7c4ff3d08bc
sidnei <sidnei.mitani@gmail.com>
2023-06-04 10:34:54 -0300

    node_modules e .env

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,19 +1,16 @@
 const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
-  await mongoose.connect(
-    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cursonodejsdicasparadev.ncg0p.mongodb.net/database?retryWrites=true&w=majority`,
-    (error) => {
-      if (error) {
-        return console.log(
-          "Ocorreu um erro ao se conectar com o banco de dados: ",
-          error
-        );
-      }
-
-      return console.log("Conexão ao banco de dados realizada com sucesso!");
+  await mongoose.connect(process.env.MONGODB_URI, (error) => {
+    if (error) {
+      return console.log(
+        "Ocorreu um erro ao se conectar com o banco de dados: ",
+        error
+      );
     }
-  );
+
+    return console.log("Conexão ao banco de dados realizada com sucesso!");
+  });
 };
 
 module.exports = connectToDatabase;
